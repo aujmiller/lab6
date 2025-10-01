7ded4376dd404f0227943e1b783601278b5257b1
Austin Miller <156855264+aujmiller@users.noreply.github.com>
2025-10-01 13:35:02 +0000

    changed assert lines in test_sample.py

--- presidio-anonymizer/tests/test_sample.py
+++ presidio-anonymizer/tests/test_sample.py
@@ -4,13 +4,18 @@ from presidio_anonymizer.sample import sample_run_anonymizer
 def test_sample_run_anonymizer():
     text = "My name is Bond."
     start, end = 11, 15
-    engine_result = sample_run_anonymizer(text, start, end)
+    result = sample_run_anonymizer(text, start, end)
 
-    assert engine_result.text == "My name is BIP."
-    assert len(engine_result.items) == 1
-    item = engine_result.items[0]
-    assert item.start == 11
-    assert item.end == 14
-    assert item.entity_type == "PERSON"
-    assert item.text == "BIP"
-    assert item.operator == "replace"
+    assert result.text == "My name is BIP."
+    assert len(result.items) == 1
+    item = result.items[0]
+    # assert item.start == 11
+    assert result.items[0].start == 11
+    # assert item.end == 14
+    assert result.items[0].end == 14
+    # assert item.entity_type == "PERSON"
+    assert result.items[0].entity_type == "PERSON"
+    # assert item.text == "BIP"
+    assert result.items[0].text == "BIP"
+    # assert item.operator == "replace"
+    assert result.items[0].operator == "replace"
