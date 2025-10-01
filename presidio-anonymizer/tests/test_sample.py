a8038be3ee1ec5e85a7a8ad387e144ccd7284379
Austin Miller <156855264+aujmiller@users.noreply.github.com>
2025-10-01 23:41:36 +0000

    changed function call

--- presidio-anonymizer/tests/test_sample.py
+++ presidio-anonymizer/tests/test_sample.py
@@ -1,16 +1,10 @@
 import pytest
 from presidio_anonymizer.sample import sample_run_anonymizer
 
-@pytest.mark.parametrize(
-    "text, start, end",
-    [
-        ("My name is Bond.", 11, 15),
-    ],
-)
-def test_sample_run_anonymizer(text, start, end):
+def test_sample_run_anonymizer():
     # text = "My name is Bond."
     # start, end = 11, 15
-    result = sample_run_anonymizer(text, start, end)
+    result = sample_run_anonymizer("My name is Bond.", 11, 15)
 
     assert result.text == "My name is BIP."
     assert len(result.items) == 1
